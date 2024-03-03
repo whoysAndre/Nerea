80030764c6e44a463f6690fdc8570f271365a897
whoysAndre <yellowcata2121@gmail.com>
2024-03-03 10:59:39 -0500

    Actualizado

--- Nerea-ts/src/App.tsx
+++ Nerea-ts/src/App.tsx
@@ -1,22 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import { AppRoutes } from "./routes/AppRoutes"
 
-function App() {
-<<<<<<< HEAD
-  return (
-    <>
-      <p>no borres</p>
-      <h1>hola</h1>
-=======
-  
-  return (
-    <>
 
+function App() {
+  return (
+    <>
       <Routes>
-        <Route path="/*" element = {<AppRoutes/>}/> 
+        <Route path="/*" element = {<AppRoutes/>}/>
       </Routes>
-      
->>>>>>> 652f0c46fd3b145b8421ced18e205c263022c707
     </>
   )
 }

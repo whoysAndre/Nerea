f8d36dc51db05ed9969de64b97b71ab40b3e0787
Adrian <toledosandro59@gmail.com>
2024-03-04 16:31:55 -0500

    cambios en la home page

--- Nerea-ts/src/nerea/components/carrusel/Carrusel.tsx
+++ Nerea-ts/src/nerea/components/carrusel/Carrusel.tsx
@@ -1,8 +1,14 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation, Autoplay } from "swiper/modules";
+import {
+  Pagination,
+  Navigation,
+  Autoplay,
+  EffectCoverflow,
+} from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
 import "swiper/css/navigation";
 import "./Carrusel.css";
 import { initialData } from "../../../seed/seed";
@@ -14,27 +20,56 @@ export const Carrusel = () => {
     <section className="ultimo">
       <div className="tittle">Ultimos lanzamientos</div>
       <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
         slidesPerView={1}
         spaceBetween={30}
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
         }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        }}
         loop={true}
         pagination={{
           clickable: true,
         }}
         navigation={true}
-        modules={[Pagination, Navigation, Autoplay]}
+        modules={[Pagination, Navigation, Autoplay, EffectCoverflow]}
         className="mySwiper swiper-last"
       >
         {products.map((product) => (
           <SwiperSlide className="sw-s-last" key={product.slug}>
             <div className="container-products">
               <img src={product.imagen} alt="" />
-              <h3>{product.titulo}</h3>
-              <h4>{product.descripcion}</h4>
-              <h5>{product.precio}</h5>
+              <div className="container-letters">
+                <h3>{product.titulo}</h3>
+                <h4>{product.descripcion}</h4>
+                <h4>{product.precio}</h4>
+              </div>
+              <button className="btn-last" type="button">
+                Ver product
+              </button>
             </div>
           </SwiperSlide>
         ))}
--- Nerea-ts/src/seed/seed.ts
+++ Nerea-ts/src/seed/seed.ts
@@ -50,6 +50,72 @@ export const initialData: SeedData = {
       descripcion: "Rel. Floral",
       categoria: 'parafina'
     },
+    {
+      titulo: "Vela Canister P.",
+      slug: 'vela_canister_parafina',
+      imagen: "/public/img/Parafina/Canister.jpg",
+      precio: 18,
+      descripcion: "Rel. Floral",
+      categoria: 'parafina'
+    },
+    {
+      titulo: "Vela Cuadrada P.",
+      slug: "vela_cuadrada_parafina_canister",
+      imagen: "/public/img/Parafina/Canister.jpg",
+      precio: 13.5,
+      descripcion: "Rosas",
+      categoria: 'parafina'
+
+    },
+    {
+      titulo: "Vela Vaso Redondo P.",
+      slug: "vela_vaso_redondo_parafina",
+      imagen: "/public/img/Parafina/Canister.jpg",
+      precio: 8,
+      descripcion: "Rel. Floral",
+      categoria: 'parafina'
+    },
+    {
+      titulo: "Vela Tulipan P.",
+      slug: "vela_tulipan_parafina_canister",
+      imagen: "/public/img/Parafina/Canister.jpg",
+      precio: 12,
+      descripcion: "Rel. Floral",
+      categoria: 'parafina'
+    },
+    {
+      titulo: "Vela Canister P.",
+      slug: 'vela_canister_parafina',
+      imagen: "/public/img/Parafina/Canister.jpg",
+      precio: 18,
+      descripcion: "Rel. Floral",
+      categoria: 'parafina'
+    },
+    {
+      titulo: "Vela Cuadrada P.",
+      slug: "vela_cuadrada_parafina_canister",
+      imagen: "/public/img/Parafina/Canister.jpg",
+      precio: 13.5,
+      descripcion: "Rosas",
+      categoria: 'parafina'
+
+    },
+    {
+      titulo: "Vela Vaso Redondo P.",
+      slug: "vela_vaso_redondo_parafina",
+      imagen: "/public/img/Parafina/Canister.jpg",
+      precio: 8,
+      descripcion: "Rel. Floral",
+      categoria: 'parafina'
+    },
+    {
+      titulo: "Vela Tulipan P.",
+      slug: "vela_tulipan_parafina_canister",
+      imagen: "/public/img/Parafina/Canister.jpg",
+      precio: 12,
+      descripcion: "Rel. Floral",
+      categoria: 'parafina'
+    },
   ]
 
 }

04d3cb5b09dea30b3c867a129791aa01c7d814f5
Adrian <toledosandro59@gmail.com>
2024-03-06 22:40:11 -0500

    Implementacion del responsive en vista producto unitario

--- Nerea-ts/src/nerea/components/product/Product.tsx
+++ Nerea-ts/src/nerea/components/product/Product.tsx
@@ -12,15 +12,32 @@ export const Product = ({ product }: Props) => {
       <div className="img-cont">
         <img src={product.imagen} alt="" className="img-product" />
       </div>
-      <div className="letters-cont">
-        <span>Nombre: {product.titulo}</span>
-        <p>Aroma: {product.descripcion}</p>
-        <p>Categoria: {product.categoria}</p>
-        <p>Precio: S/{product.precio}</p>
+      <div className="container-seccion2">
+        <div className="letters-cont">
+          <span>{product.titulo}</span>
+          <p>Aroma: {product.descripcion}</p>
+          <p>Categoria: {product.categoria}</p>
+          <p>Precio: S/{product.precio}</p>
+        </div>
+        <div className="container-btn">
+          <div className="cantidad">
+            <button className="btnMinus">
+              <span className="span-Minus">
+                <img src="../../../../public/minus.svg" alt="Minus" />
+              </span>
+            </button>
+            <span className="span-cantidad">1</span>
+            <button type="button" className="btnPlus">
+              <span className="span-plus">
+                <img src="../../../../public/plus.svg" alt="Minus" />
+              </span>
+            </button>
+          </div>
+          <Link to={`/`} className="btn-comprar" type="button">
+            Comprar
+          </Link>
+        </div>
       </div>
-      <Link to={`/`} className="btn-comprar" type="button">
-        Comprar
-      </Link>
     </div>
   );
 };
